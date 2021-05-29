97e67c9e716dfcd37f2f9769c843c3acfa0a64f4
Jae-G <dbswo343434@gmail.com>
2021-05-29 21:12:49 +0900

    add Banner

---
+++ src/pages/Banner.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-parallax
+    class="text-h2 "
+    max-height="400px"
+    :src="require('@/assets/banner.jpg')"
+    align="center"
+    justify="center"
+    dark
+  >
+    <p>
+      안녕하세요.
+    </p>
+    <p>
+      Front End 개발자입니다
+    </p>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
