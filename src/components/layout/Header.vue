25645ec2010034c9c0bc53f2e38805c1e9a5d36d
Jae-G <dbswo343434@gmail.com>
2021-05-29 21:11:50 +0900

    add Header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar
+    app
+    dark
+  >
+    <v-tabs>
+      <v-tab>
+        프로필
+      </v-tab>
+      <v-tab>
+        포트폴리오
+      </v-tab>
+      <v-tab>
+        연락처
+      </v-tab>
+    </v-tabs>
+    <v-btn>
+      <v-icon>
+        mdi-github
+      </v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
