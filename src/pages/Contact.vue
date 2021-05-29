f70fc22c7186bee53eb9503488e537f528371a26
Jae-G <dbswo343434@gmail.com>
2021-05-29 20:23:31 +0900

    add Contact

---
+++ src/pages/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    align="center"
+    justify="center"
+  >
+    <v-card>
+      contact <br>
+
+      +82 010 2957 7409 <br>
+      dbswo343434@gmail.com
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
