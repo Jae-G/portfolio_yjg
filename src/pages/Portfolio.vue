2b96ca1990a22be275006c0116cc8e6e6b964635
Jae-G <dbswo343434@gmail.com>
2021-05-29 20:16:29 +0900

    add Portfolio

---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-card flat>
+      <v-card-title class="text-h4">
+        포트폴리오
+      </v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
