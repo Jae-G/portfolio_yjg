d6b869d77f857af5d9ae19561c612d712e4eaa2c
Jae-G <dbswo343434@gmail.com>
2021-05-29 21:12:34 +0900

    feature connect Component in Index

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
+  <v-app>
     <Header />
-  </div>
+    <v-main>
+      <Banner />
+      <v-container>
+        <Profile />
+        <Portfolio />
+      </v-container>
+    </v-main>
+    <Contact />
+    <Footer />
+  </v-app>
 </template>
 
 <script>
-import Header from '@/components/Header'
+import Header from '@/components/layout/Header'
+import Banner from '@/pages/Banner'
+import Profile from '@/pages/Profile'
+import Portfolio from '@/pages/Portfolio'
+import Contact from '@/pages/Contact'
+import Footer from '@/components/layout/Footer'
+
 export default {
   components : {
-    Header
+    Header,Footer,Profile,
+    Portfolio,Banner,Contact
   }
 }
 </script>
