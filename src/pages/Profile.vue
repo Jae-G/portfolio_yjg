3a9cb3f90bdde0a19a9584956e016b31fd8fd9e1
Jae-G <dbswo343434@gmail.com>
2021-05-30 23:15:44 +0900

    fix Profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -2,7 +2,7 @@
   <div>
     <v-card flat>
       <v-card-title class="text-h4">
-        프로필
+        Profile
       </v-card-title>
       <v-card-text>
         <v-row>
@@ -17,15 +17,15 @@
             cols="9"
             justify="start"
           >
-            <div class="text-h4">
+            <p class="text-h4">
               윤재근
-            </div>
+            </p>
 
-            <div>
-              웹 프론트엔드 엔지니어 윤재근 입니다. 여러 프로젝트를 수행하면서 개발 역량을 쌓아왔고, 현재는 삼성SDS TMS사이트 유지보수 및 개발 업무를 수행하고 있습니다. <br><br>
-
+            <p>
+              웹 프론트엔드 엔지니어 윤재근 입니다. 여러 프로젝트를 수행하면서 개발 역량을 쌓아왔고, 현재는 삼성SDS TMS사이트 유지보수 및 개발 업무를 수행하고 있습니다.
+              <br>
               자기개발에 손을 놓지 않습니다. 퇴근 후는 사이드프로젝트를 하며 공부합니다. 개인적으로 진행하는 프로젝트와 팀원들과 협업하는 프로젝트를 병행하고 있습니다.
-            </div>
+            </p>
           </v-col>
         </v-row>
       </v-card-text>
